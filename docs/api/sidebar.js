1afe622f2e885149cb84ae84716a2d7f021f2423
fenjalien <34489450+fenjalien@users.noreply.github.com>
2024-07-01 16:11:07 +0100

    Add shapes docs

---
+++ docs/api/sidebar.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    type: "category",
+    label: "Draw Functions",
+    link: {
+      type: "doc",
+      id: "api/draw-functions/index",
+    },
+    items: [
+      {
+        type: "category",
+        label: "Shapes",
+        link: {
+          type: "doc",
+          id: "api/draw-functions/shapes/index",
+        },
+        items: [
+          "api/draw-functions/shapes/circle",
+          "api/draw-functions/shapes/arc",
+          "api/draw-functions/shapes/mark",
+          "api/draw-functions/shapes/line",
+          "api/draw-functions/shapes/grid",
+          "api/draw-functions/shapes/content",
+          "api/draw-functions/shapes/rect",
+          "api/draw-functions/shapes/bezier",
+          "api/draw-functions/shapes/hobby",
+          "api/draw-functions/shapes/catmull",
+          "api/draw-functions/shapes/merge-path",
+        ],
+      },
+    ],
+  },
+];
