97a55d3cd12ab54401c4efb1d725d940c8cc9ce6
Rik Huijzer <github@huijzer.xyz>
2025-03-23 16:34:40 +0100

    Switch to f64

--- cetz-core/src/lib.rs
+++ cetz-core/src/lib.rs
@@ -1,14 +1,17 @@
-use wasm_minimal_protocol::*;
 use ciborium::de::from_reader;
 use ciborium::ser::into_writer;
 use serde::Deserialize;
+use wasm_minimal_protocol::*;
 
 initiate_protocol!();
 
-type Point = Vec<f32>;
+type Point = Vec<f64>;
 
-fn round(x: f32, digits: u32) -> f32 {
-    let factor = 10.0_f32.powi(digits as i32);
+/// Round `x` to `digits` decimal places.
+///
+/// This uses the same algorithm as Typst does.
+fn round(x: f64, digits: u32) -> f64 {
+    let factor = 10.0_f64.powi(digits as i32);
     (x * factor).round() / factor
 }
 
@@ -16,47 +19,47 @@ fn vector_add(a: Point, b: Point) -> Point {
     a.iter().zip(b.iter()).map(|(a, b)| a + b).collect()
 }
 
-fn vector_scale(a: Point, b: f32) -> Point {
+fn vector_scale(a: Point, b: f64) -> Point {
     a.iter().map(|a| a * b).collect()
 }
 
-fn cubic_point(a: Point, b: Point, c1: Point, c2: Point, t: f32) -> Point {
-  // (1-t)^3*a + 3*(1-t)^2*t*c1 + 3*(1-t)*t^2*c2 + t^3*b
-  vector_add(
+fn cubic_point(a: Point, b: Point, c1: Point, c2: Point, t: f64) -> Point {
+    // (1-t)^3*a + 3*(1-t)^2*t*c1 + 3*(1-t)*t^2*c2 + t^3*b
     vector_add(
-      vector_scale(a, (1.0 - t).powi(3)),
-      vector_scale(c1, 3.0 * (1.0 - t).powi(2) * t)
-    ),
-    vector_add(
-      vector_scale(c2, 3.0 * (1.0 - t) * t.powi(2)),
-      vector_scale(b, t.powi(3))
+        vector_add(
+            vector_scale(a, (1.0 - t).powi(3)),
+            vector_scale(c1, 3.0 * (1.0 - t).powi(2) * t),
+        ),
+        vector_add(
+            vector_scale(c2, 3.0 * (1.0 - t) * t.powi(2)),
+            vector_scale(b, t.powi(3)),
+        ),
     )
-  )
 }
 
 /// Compute roots of a single dimension (x, y, z) of the
 /// curve by using the abc formula for finding roots of
 /// the curves first derivative.
-fn dim_extrema(a: f32, b: f32, c1: f32, c2: f32) -> Vec<f32> {
+fn dim_extrema(a: f64, b: f64, c1: f64, c2: f64) -> Vec<f64> {
     let f0 = round(3.0 * (c1 - a), 8);
     let f1 = round(6.0 * (c2 - 2.0 * c1 + a), 8);
     let f2 = round(3.0 * (b - 3.0 * c2 + 3.0 * c1 - a), 8);
 
     if f1 == 0.0 && f2 == 0.0 {
-        return vec![]
+        return vec![];
     }
 
     if f2 == 0.0 {
-        return vec![-f0 / f1]
+        return vec![-f0 / f1];
     }
 
     let d = f1 * f1 - 4.0 * f0 * f2;
     if d < 0.0 {
-        return vec![]
+        return vec![];
     }
 
     if d == 0.0 {
-        return vec![-f1 / (2.0 * f2)]
+        return vec![-f1 / (2.0 * f2)];
     }
 
     let sqrt_d = d.sqrt();
